64ef380f8d96055238af640666106de2aac6414f
jeproch <jprevenant@gmail.com>
2023-09-19 18:41:12 -0700

    major commit for home page and buttons

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Page loaded')
+})
+
+//All elements
+
+let contactBtn = document.getElementById("contact-btn")
+let installBtn = document.getElementById("install-btn")
+let communityBtn = document.getElementById("community-btn")
+let supportBtn = document.getElementById("support-btn")
+
+// procedures/functions
+
+contactBtn.addEventListener('click', function() {
+    //For contact button
+})
