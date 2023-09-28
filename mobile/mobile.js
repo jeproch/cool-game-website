fb77f7b3570a345f31fa96fbbfb9d81559de0f77
jeproch <jprevenant@gmail.com>
2023-09-27 23:00:19 -0700

    Cool commit for hovering over paragraph

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -14,3 +14,25 @@ menuBtn.addEventListener('click', function() {
     notMenu.classList.add("hide")
 })
 
+
+//only for first paragraph!!
+
+// Get a reference to the element you want to add the class to
+const paragraph = document.getElementById('paragraph');
+
+// Add a touchstart event listener to detect when the user taps on the element
+paragraph.addEventListener('touchstart', function(event) {
+    // Prevent the default touchstart behavior (e.g., scrolling)
+    event.preventDefault();
+
+    // Add the CSS class when the user taps on the paragraph
+    paragraph.classList.add('hover-paragraph');
+});
+
+// Add a touchend event listener to detect when the user stops touching the paragraph
+paragraph.addEventListener('touchend', function(event) {
+    // Remove the CSS class when the user stops touching the paragraph
+    paragraph.classList.remove('hover-paragraph');
+});
+
+//only for first paragraph!!
